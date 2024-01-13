cc3665696be76b449a3a3c8cbf5b7490bf59fa55
Alexander Jang <atjang@berkeley.edu>
2024-01-13 15:24:09 -0600

    Updates

---
+++ precache-manifest.42f4c2ef1bf546de6d5a488e0a5c8a53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155df5cfbfd099b10a19cc18f2e1e862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "633535cb8346d40e3d44",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633535cb8346d40e3d44",
+    "url": "/static/js/main.e1d2fa6d.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "0a66aa25b20efee80ba45f01102a88e3",
+    "url": "/static/media/alexanderjang.0a66aa25.png"
+  },
+  {
+    "revision": "9dc39272fec02f79ee279869a828319a",
+    "url": "/static/media/resume.9dc39272.pdf"
+  }
+]);
